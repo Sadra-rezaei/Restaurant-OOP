179b882f2586039756676e4248d525559b08ef1a
Sadra <sadrarezaei4@gmail.com>
2024-11-19 11:37:38 +0330

    table.java updated

--- Table.java
+++ Table.java
@@ -1,20 +1,33 @@
+import java.util.ArrayList;
+
 public class Table {
     private int number;
-    private int capacity;
+    private ArrayList<Chair> chairs;
     private boolean full;
 
     public Table(int capacity, int number) {
         this.number = number;
-        this.capacity = capacity;
         this.full = false;
+        this.chairs = new ArrayList<>();
+        for (int i = 0; i < capacity; i++) {
+            this.chairs.add(new Chair());
+        }
     }
 
     public int getNumber() {
         return number;
     }
 
+    public ArrayList<Chair> getChairs() {
+        return chairs;
+    }
+
     public int getCapacity() {
-        return capacity;
+        return chairs.size();
+    }
+
+    public void addChair(Chair chair) {
+        chairs.add(chair);
     }
 
     public boolean isFull() {
